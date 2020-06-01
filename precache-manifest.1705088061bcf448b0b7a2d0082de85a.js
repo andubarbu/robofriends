03d56fdc3dd4fee612e4ef720006777ad01e1a00
andubarbu <andubarbu@gmail.com>
2020-06-01 19:12:18 +0100

    Updates

---
+++ precache-manifest.1705088061bcf448b0b7a2d0082de85a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa7fada1455744b87548d55420d2cf9",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "9b19f60f488f468b256e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8a495ef4953c6d253884",
+    "url": "/robofriends/static/css/main.ea038b58.chunk.css"
+  },
+  {
+    "revision": "9b19f60f488f468b256e",
+    "url": "/robofriends/static/js/2.95752d7b.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/robofriends/static/js/2.95752d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a495ef4953c6d253884",
+    "url": "/robofriends/static/js/main.1de44e92.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
