1ab1f4c4416c1f908c7efc8ca033dd6978ca514b
andubarbu <andubarbu@gmail.com>
2020-06-01 18:30:42 +0100

    fixed folder structure + README

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../Components/CardList';
+import SearchBox from '../Components/SearchBox';
 import './App.css';
-import Scroll from './Scroll';
+import Scroll from '../Components/Scroll';
 
 class App extends Component {
     constructor() {
@@ -24,10 +24,11 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
-        if(this.state.robots.length === 0){
+        if(!robots.length){
             return <h1 className='f1 mt5 navy tc'>Loading, please wait.</h1>
         }
         else{
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import App from './App';
+import App from './Containers/App';
 
 ReactDOM.render(
   <React.StrictMode>
